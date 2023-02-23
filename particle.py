9e27d1ba27ad9017cab822033332cf7588b80839
Francis Osei Annin <francisannin5@gmail.com>
2023-02-23 13:10:26 +0000

    Added linear distance_type1, distance_type2, and linear velocity methods.

--- particle.py
+++ particle.py
@@ -1,6 +1,7 @@
 """
 particle type for linear motion.
 mass and volume of particle are negligible.
+fundamental equation of
 """
 from utils.generics import delta
 
@@ -9,8 +10,12 @@ class Particle(object):
     """
     standard symbols for quantities
     initial velocity (u)
-    final velocity (v)
+    final velocity(v)
+    time duration(t)
+    linear distance(s)
+
     """
+
     def __int__(self, initial_velocity, final_velocity, duration_in_seconds):
         self.init_velocity = initial_velocity
         self.fin_velocity = final_velocity
@@ -24,12 +29,31 @@ class Particle(object):
         acceleration = delta(self.init_velocity, self.fin_velocity) / self.dur
         return acceleration
 
-    def linear_distance(self):
+    def linear_distance_type1(self):
         """
-        returns the linear distance(s) using the equation (v^2 - u^2)/2a
+        returns the linear distance(s) using the equation s = (v^2 - u^2)/2a
+        where all variable in equation assume their standard meaning
         :return:
         """
-        lin_dist = (self.final_velocity**2 - self.init_velocity**2) / 2 * self.acceleration()
+        lin_dist = (self.final_velocity ** 2 - self.init_velocity ** 2) / 2 * self.acceleration()
         return lin_dist
 
+    def linear_distance_type2(self):
+        """
+        returns the linear distance(s) using the equation s = ut  + 1/2at^2
+        where all variable in equations assume their standard meaning
+        :return:
+        """
+        lin_dist = (self.init_velocity * self.dur) + (self.acceleration() * self.dur ** 2) / 2
+        return lin_dist
 
+    def linear_velocity(self, initial_time, final_time):
+        """
+        returns the linear distance given the initial and final time
+        :param initial_time:
+        :param final_time:
+        :return:
+        """
+        change_in_time = delta(initial_time, final_time)
+        change_in_velocity = delta(self.init_velocity, self.fin_velocity)
+        return change_in_velocity/change_in_time
